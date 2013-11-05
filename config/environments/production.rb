92a344725fb9dd0d27783577bc4351f171aba456
Quest Henkart <qhenkart@gmail.com>
2013-11-05 12:35:57 -0800

    added another bucket inside credentials, removed the intializer I previously added... still troubleshooting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,7 @@ Questbook::Application.configure do
     :storage => :s3,
     :bucket => ENV['AWS_BUCKET'],
     :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
