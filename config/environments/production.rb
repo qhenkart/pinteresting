9c0317a14606158c77b8e9c9ddf110bbc2eaa5f6
Quest Henkart <qhenkart@gmail.com>
2013-11-05 10:48:03 -0800

    changed a production field to troubleshoot heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,11 +83,11 @@ Questbook::Application.configure do
 
 #aws, required to store images on heroku
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 end
